6eec605f9bbb3d3ee07971ec632852a7d7dc04c9
Roamin <roamin@tuta.io>
2023-02-08 18:26:49 +0000

    add more tests

--- contracts/name-service/tests/integration.spec.ts
+++ contracts/name-service/tests/integration.spec.ts
@@ -32,91 +32,224 @@ afterAll(async () => {
   await localKoinos.stopNode();
 });
 
-test("registration", async () => {
-  const [genesis, koin, nameserviceAcct, koinDomainAcct, doedotkoinDomainAcct, user1] = localKoinos.getAccounts();
+describe('mint', () => {
+  test("it should mint names and sub names", async () => {
+    const [genesis, koin, nameserviceAcct, koinDomainAcct, doedotkoinDomainAcct, user1] = localKoinos.getAccounts();
 
-  // deploy nameservice 
-  // @ts-ignore abi is compatible
-  const nameserviceContract = await localKoinos.deployContract(nameserviceAcct.wif, './build/debug/contract.wasm', abi);
+    // deploy nameservice 
+    // @ts-ignore abi is compatible
+    const nameserviceContract = await localKoinos.deployContract(nameserviceAcct.wif, './build/debug/contract.wasm', abi);
 
-  // deploy koindomain  
-  // @ts-ignore abi is compatible
-  const koindomainContract = await localKoinos.deployContract(koinDomainAcct.wif, '../test-domain/build/debug/contract.wasm', abi);
+    // deploy koindomain  
+    // @ts-ignore abi is compatible
+    const koindomainContract = await localKoinos.deployContract(koinDomainAcct.wif, '../test-domain/build/debug/contract.wasm', abi);
 
-  // deploy doe.koin  
-  // @ts-ignore abi is compatible
-  const doedotkoinDomainContract = await localKoinos.deployContract(doedotkoinDomainAcct.wif, '../test-domain/build/debug/contract.wasm', abi);
+    // deploy doe.koin  
+    // @ts-ignore abi is compatible
+    const doedotkoinDomainContract = await localKoinos.deployContract(doedotkoinDomainAcct.wif, '../test-domain/build/debug/contract.wasm', abi);
 
+    // ASCII characters
+    let res = await nameserviceContract.functions.mint({
+      name: 'koin',
+      owner: koinDomainAcct.address
+    });
 
-  let res = await nameserviceContract.functions.mint({
-    name: 'koin',
-    owner: koinDomainAcct.address
-  });
+    await res.transaction?.wait();
 
-  await res.transaction?.wait();
+    res = await nameserviceContract.functions.get_name({
+      name: 'koin',
+    });
 
-  res = await nameserviceContract.functions.get_name({
-    name: 'koin',
-  });
+    expect(res?.result?.domain).toBe(undefined);
+    expect(res?.result?.name).toBe('koin');
+    expect(res?.result?.owner).toBe(koinDomainAcct.address);
+    expect(res?.result?.expiration).toBe('0');
+    expect(res?.result?.grace_period_end).toBe('0');
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
 
-  expect(res?.result?.name).toBe('koin');
-  expect(res?.result?.owner).toBe(koinDomainAcct.address);
-  expect(res?.result?.expiration).toBe('0');
-  expect(res?.result?.grace_period_end).toBe('0');
+    const durationIncrements = 3;
 
-  res = await nameserviceContract.functions.mint({
-    name: 'doe.koin',
-    duration_increments: 3,
-    owner: doedotkoinDomainAcct.address,
-    payment_from: doedotkoinDomainAcct.address,
-    payment_token_address: koin.address
-  });
-
-  await res.transaction?.wait();
-
-  res = await nameserviceContract.functions.get_name({
-    name: 'doe.koin',
-  });
-
-  expect(res?.result?.name).toBe('doe');
-  expect(res?.result?.domain).toBe('koin');
-  expect(res?.result?.owner).toBe(doedotkoinDomainAcct.address);
-
-  res = await nameserviceContract.functions.mint({
-    name: 'john.doe.koin',
-    duration_increments: 3,
-    owner: user1.address,
-    payment_from: user1.address,
-    payment_token_address: koin.address
-  });
-
-  await res.transaction?.wait();
-
-  res = await nameserviceContract.functions.get_name({
-    name: 'john.doe.koin',
-  });
-
-  expect(res?.result?.name).toBe('john');
-  expect(res?.result?.domain).toBe('doe.koin');
-  expect(res?.result?.owner).toBe(user1.address);
-
-  try {
     res = await nameserviceContract.functions.mint({
-      name: 'al.doe.koin',
+      name: 'doe.koin',
+      duration_increments: durationIncrements,
+      owner: doedotkoinDomainAcct.address,
+      payment_from: doedotkoinDomainAcct.address,
+      payment_token_address: koin.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: 'doe.koin',
+    });
+
+    expect(res?.result?.domain).toBe('koin');
+    expect(res?.result?.name).toBe('doe');
+    expect(res?.result?.owner).toBe(doedotkoinDomainAcct.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    // check sub_names_count incremented on koin domain
+    res = await nameserviceContract.functions.get_name({
+      name: 'koin',
+    });
+
+    expect(res?.result?.sub_names_count).toBe('1');
+
+    res = await nameserviceContract.functions.mint({
+      name: 'john.doe.koin',
       duration_increments: 3,
       owner: user1.address,
       payment_from: user1.address,
       payment_token_address: koin.address
     });
-  
+
     await res.transaction?.wait();
 
-    res = await nameserviceContract.functions.get_names({
-      owner: user1.address,
+    res = await nameserviceContract.functions.get_name({
+      name: 'john.doe.koin',
     });
 
-    console.log(res.result);
-  } catch (error) {
-    console.log(error);
-  }
+    expect(res?.result?.domain).toBe('doe.koin');
+    expect(res?.result?.name).toBe('john');
+    expect(res?.result?.owner).toBe(user1.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    // emojis
+    res = await nameserviceContract.functions.mint({
+      name: '💎',
+      owner: koinDomainAcct.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '💎',
+    });
+
+    expect(res?.result?.domain).toBe(undefined);
+    expect(res?.result?.name).toBe('💎');
+    expect(res?.result?.owner).toBe(koinDomainAcct.address);
+    expect(res?.result?.expiration).toBe('0');
+    expect(res?.result?.grace_period_end).toBe('0');
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    
+    res = await nameserviceContract.functions.mint({
+      name: '🔥.💎',
+      duration_increments: 3,
+      owner: doedotkoinDomainAcct.address,
+      payment_from: doedotkoinDomainAcct.address,
+      payment_token_address: koin.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '🔥.💎',
+    });
+
+    expect(res?.result?.domain).toBe('💎');
+    expect(res?.result?.name).toBe('🔥');
+    expect(res?.result?.owner).toBe(doedotkoinDomainAcct.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    res = await nameserviceContract.functions.mint({
+      name: '❤️.🔥.💎',
+      duration_increments: 3,
+      owner: user1.address,
+      payment_from: user1.address,
+      payment_token_address: koin.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '❤️.🔥.💎',
+    });
+
+    expect(res?.result?.domain).toBe('🔥.💎');
+    expect(res?.result?.name).toBe('❤️');
+    expect(res?.result?.owner).toBe(user1.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    // Chinese characters
+    res = await nameserviceContract.functions.mint({
+      name: '钻石',
+      owner: koinDomainAcct.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '钻石',
+    });
+
+    expect(res?.result?.domain).toBe(undefined);
+    expect(res?.result?.name).toBe('钻石');
+    expect(res?.result?.owner).toBe(koinDomainAcct.address);
+    expect(res?.result?.expiration).toBe('0');
+    expect(res?.result?.grace_period_end).toBe('0');
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    
+    res = await nameserviceContract.functions.mint({
+      name: '火.钻石',
+      duration_increments: 3,
+      owner: doedotkoinDomainAcct.address,
+      payment_from: doedotkoinDomainAcct.address,
+      payment_token_address: koin.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '火.钻石',
+    });
+
+    expect(res?.result?.domain).toBe('钻石');
+    expect(res?.result?.name).toBe('火');
+    expect(res?.result?.owner).toBe(doedotkoinDomainAcct.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+
+    res = await nameserviceContract.functions.mint({
+      name: '心形物.火.钻石',
+      duration_increments: 3,
+      owner: user1.address,
+      payment_from: user1.address,
+      payment_token_address: koin.address
+    });
+
+    await res.transaction?.wait();
+
+    res = await nameserviceContract.functions.get_name({
+      name: '心形物.火.钻石',
+    });
+
+    expect(res?.result?.domain).toBe('火.钻石');
+    expect(res?.result?.name).toBe('心形物');
+    expect(res?.result?.owner).toBe(user1.address);
+    expect(res?.result?.expiration).toBe(`${durationIncrements * 1770429035204 * 2}`);
+    expect(res?.result?.grace_period_end).toBe(`${durationIncrements * 1770429035204 * 3}`);
+    expect(res?.result?.sub_names_count).toBe('0');
+    expect(res?.result?.locked_kap_tokens).toBe('0');
+  });
 });
+
--- contracts/test-domain/assembly/Testdomain.ts
+++ contracts/test-domain/assembly/Testdomain.ts
@@ -1,19 +1,27 @@
+import { System } from "@koinos/sdk-as";
 import { testdomain } from "./proto/testdomain";
 
 export class Testdomain {
   authorize_mint(
     args: testdomain.authorize_mint_arguments
   ): testdomain.authorize_mint_result {
-    // const name = args.name;
-    // const domain = args.domain;
-    // const duration_increments = args.duration_increments;
-    // const owner = args.owner;
-    // const payment_from = args.payment_from;
-    // const payment_token_address = args.payment_token_address;
+    const name = args.name;
+    const domain = args.domain;
+    const duration_increments = args.duration_increments;
+    const owner = args.owner;
+    const payment_from = args.payment_from;
+    const payment_token_address = args.payment_token_address;
+
+    System.require(name.length > 0, 'name argument was not provided');
+    System.require(domain.length > 0, 'domain argument was not provided');
+    System.require(duration_increments > 0, 'duration_increments argument was not provided');
+    System.require(owner.length > 0, 'owner argument was not provided');
+    System.require(payment_from.length > 0, 'payment_from argument was not provided');
+    System.require(payment_token_address.length > 0, 'payment_token_address argument was not provided');
 
     const res = new testdomain.authorize_mint_result();
-    res.expiration = 1770429035203;
-    res.grace_period_end = 1770429035204;
+    res.expiration = 1770429035204 * 2 * duration_increments;
+    res.grace_period_end = 1770429035204 * 3 * duration_increments;
 
     return res;
   }
