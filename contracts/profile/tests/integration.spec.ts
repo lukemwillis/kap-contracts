fe8119aa1d999851d56ec095dde3c25b08ad821f
luke <luke@prescryptive.com>
2023-07-14 14:07:43 -0700

    format files & fix deploy scripts

--- contracts/profile/tests/integration.spec.ts
+++ contracts/profile/tests/integration.spec.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { Contract, LocalKoinos, Serializer, Token } from '@roamin/local-koinos';
+import { Contract, LocalKoinos, Serializer, Token } from "@roamin/local-koinos";
 
-import * as profileAbi from '../abi/profile-abi.json';
-import * as nameserviceAbi from '../../name-service/abi/nameservice-abi.json';
-import * as collectionAbi from '../../test-collection/abi/testcollection-abi.json';
+import * as profileAbi from "../abi/profile-abi.json";
+import * as nameserviceAbi from "../../name-service/abi/collections-abi.json";
+import * as collectionAbi from "../../test-collection/abi/testcollection-abi.json";
 
 // @ts-ignore koilib_types is needed when using koilib
 profileAbi.koilib_types = profileAbi.types;
@@ -16,13 +16,12 @@ collectionAbi.koilib_types = collectionAbi.types;
 
 jest.setTimeout(600000);
 
-
 let localKoinos = new LocalKoinos();
 
-if (process.env.ENV === 'LOCAL') {
+if (process.env.ENV === "LOCAL") {
   localKoinos = new LocalKoinos({
-    rpc: 'http://host.docker.internal:8080',
-    amqp: 'amqp://host.docker.internal:5672'
+    rpc: "http://host.docker.internal:8080",
+    amqp: "amqp://host.docker.internal:5672",
   });
 }
 
@@ -46,27 +45,39 @@ beforeAll(async () => {
   // start local-koinos node
   await localKoinos.startNode();
 
-  await localKoinos.deployKoinContract({ mode: 'manual' });
-  await localKoinos.mintKoinDefaultAccounts({ mode: 'manual' });
-  await localKoinos.deployNameServiceContract({ mode: 'manual' });
-  await localKoinos.setNameServiceRecord('koin', koin.address, { mode: 'manual' });
+  await localKoinos.deployKoinContract({ mode: "manual" });
+  await localKoinos.mintKoinDefaultAccounts({ mode: "manual" });
+  await localKoinos.deployNameServiceContract({ mode: "manual" });
+  await localKoinos.setNameServiceRecord("koin", koin.address, {
+    mode: "manual",
+  });
 
-  // deploy profile 
+  // deploy profile
   profileContract = await localKoinos.deployContract(
     profileAcct.wif,
-    './build/release/contract.wasm',
+    "./build/release/contract.wasm",
     // @ts-ignore abi is compatible
     profileAbi,
-    { mode: 'manual' },
+    { mode: "manual" }
   );
 
-  // deploy nameservice  
-  // @ts-ignore abi is compatible
-  nameserviceContract = await localKoinos.deployContract(nameserviceAcct.wif, '../name-service/build/release/contract.wasm', nameserviceAbi, { mode: 'manual' });
+  // deploy nameservice
+  nameserviceContract = await localKoinos.deployContract(
+    nameserviceAcct.wif,
+    "../name-service/build/release/contract.wasm",
+    // @ts-ignore abi is compatible
+    nameserviceAbi,
+    { mode: "manual" }
+  );
 
   // deploy test collection
-  // @ts-ignore abi is compatible
-  collectionContract = await localKoinos.deployContract(collectionAcct.wif, '../test-collection/build/release/contract.wasm', collectionAbi, { mode: 'manual' });
+  collectionContract = await localKoinos.deployContract(
+    collectionAcct.wif,
+    "../test-collection/build/release/contract.wasm",
+    // @ts-ignore abi is compatible
+    collectionAbi,
+    { mode: "manual" }
+  );
 
   await localKoinos.startBlockProduction();
 });
@@ -77,15 +88,15 @@ afterAll(async () => {
 });
 
 function encodeHex(str: string) {
-  return `0x${Buffer.from(str, 'utf-8').toString('hex')}`;
+  return `0x${Buffer.from(str, "utf-8").toString("hex")}`;
 }
 
-describe('profile', () => {
-  it('should set the contract metadata', async () => {
+describe("profile", () => {
+  it("should set the contract metadata", async () => {
     let res = await profileContract.functions.set_metadata({
       metadata: {
-        nameservice_address: nameserviceContract.getId()
-      }
+        nameservice_address: nameserviceContract.getId(),
+      },
     });
 
     await res.transaction?.wait();
@@ -95,87 +106,93 @@ describe('profile', () => {
     expect(res.result.nameservice_address).toEqual(nameserviceContract.getId());
   });
 
-  it('should set a profile', async () => {
+  it("should set a profile", async () => {
     // set a simple profile with no name and avatar
-    let res = await profileContract.functions.update_profile({
-      address: user1.address,
-      profile: {
-        bio: 'I am user 1',
-        theme: '111',
-        links: [
-          { key: 'hello', value: 'world' },
-          { key: 'hello2', value: 'world2' }
-        ]
+    let res = await profileContract.functions.update_profile(
+      {
+        address: user1.address,
+        profile: {
+          bio: "I am user 1",
+          theme: "111",
+          links: [
+            { key: "hello", value: "world" },
+            { key: "hello2", value: "world2" },
+          ],
+        },
+      },
+      {
+        beforeSend: async (tx) => {
+          await user1.signer.signTransaction(tx);
+        },
       }
-    }, {
-      beforeSend: async (tx) => {
-        await user1.signer.signTransaction(tx);
-      }
-    });
+    );
 
     await res.transaction?.wait();
 
     res = await profileContract.functions.get_profile({
-      address: user1.address
+      address: user1.address,
     });
 
     expect(res.result).toEqual({
-      avatar_contract_id: '',
-      avatar_token_id: '0x',
-      bio: 'I am user 1',
-      theme: '111',
+      avatar_contract_id: "",
+      avatar_token_id: "0x",
+      bio: "I am user 1",
+      theme: "111",
       links: [
-        { key: 'hello', value: 'world' },
-        { key: 'hello2', value: 'world2' },
-      ]
+        { key: "hello", value: "world" },
+        { key: "hello2", value: "world2" },
+      ],
     });
 
-    res = await profileContract.functions.update_profile({
-      address: user1.address,
-      profile: {
-        bio: 'I am still user 1',
-        theme: 'fff',
-        links: [
-          { key: 'hello', value: 'world' },
-          { key: 'hello2', value: 'world2' },
-          { key: 'hello3', value: 'world3' }
-        ]
+    res = await profileContract.functions.update_profile(
+      {
+        address: user1.address,
+        profile: {
+          bio: "I am still user 1",
+          theme: "fff",
+          links: [
+            { key: "hello", value: "world" },
+            { key: "hello2", value: "world2" },
+            { key: "hello3", value: "world3" },
+          ],
+        },
+      },
+      {
+        beforeSend: async (tx) => {
+          await user1.signer.signTransaction(tx);
+        },
       }
-    }, {
-      beforeSend: async (tx) => {
-        await user1.signer.signTransaction(tx);
-      }
-    });
+    );
 
     await res.transaction?.wait();
 
     res = await profileContract.functions.get_profile({
-      address: user1.address
+      address: user1.address,
     });
 
     expect(res.result).toEqual({
-      avatar_contract_id: '',
-      avatar_token_id: '0x',
-      bio: 'I am still user 1',
-      theme: 'fff',
+      avatar_contract_id: "",
+      avatar_token_id: "0x",
+      bio: "I am still user 1",
+      theme: "fff",
       links: [
-        { key: 'hello', value: 'world' },
-        { key: 'hello2', value: 'world2' },
-        { key: 'hello3', value: 'world3' }
-      ]
+        { key: "hello", value: "world" },
+        { key: "hello2", value: "world2" },
+        { key: "hello3", value: "world3" },
+      ],
     });
 
     // mint some names
     res = await nameserviceContract.functions.mint({
-      name: 'user1',
-      owner: user1.address
+      name: "user1",
+      owner: user1.address,
     });
 
     await res.transaction?.wait();
 
     res = await nameserviceContract.functions.mint({
-      name: 'user2',
-      owner: user2.address
+      name: "user2",
+      owner: user2.address,
     });
 
     await res.transaction?.wait();
@@ -183,224 +200,250 @@ describe('profile', () => {
     // mint some NFTs
     res = await collectionContract.functions.mint({
       owner: user1.address,
-      token_id: '0x01',
+      token_id: "0x01",
     });
 
     await res.transaction?.wait();
 
     res = await collectionContract.functions.mint({
       owner: user2.address,
-      token_id: '0x02',
+      token_id: "0x02",
     });
 
     await res.transaction?.wait();
 
     // set a profile with name and avatar
-    res = await profileContract.functions.update_profile({
+    res = await profileContract.functions.update_profile(
+      {
+        address: user2.address,
+        profile: {
+          avatar_contract_id: collectionContract.getId(),
+          avatar_token_id: "0x02",
+          name: "user2",
+          bio: "I am user 2",
+          theme: "CCCccc",
+          links: [
+            { key: "hello", value: "world" },
+            { key: "hello2", value: "world2" },
+          ],
+        },
+      },
+      {
+        beforeSend: async (tx) => {
+          await user2.signer.signTransaction(tx);
+        },
+      }
+    );
+
+    await res.transaction?.wait();
+
+    res = await profileContract.functions.get_profile({
       address: user2.address,
-      profile: {
-        avatar_contract_id: collectionContract.getId(),
-        avatar_token_id: '0x02',
-        name: 'user2',
-        bio: 'I am user 2',
-        theme: 'CCCccc',
-        links: [
-          { key: 'hello', value: 'world' },
-          { key: 'hello2', value: 'world2' }
-        ]
-      }
-    }, {
-      beforeSend: async (tx) => {
-        await user2.signer.signTransaction(tx);
-      }
-    });
-
-    await res.transaction?.wait();
-
-    res = await profileContract.functions.get_profile({
-      address: user2.address
     });
 
     expect(res.result).toEqual({
       avatar_contract_id: collectionContract.getId(),
-      avatar_token_id: '0x02',
-      name: 'user2',
-      bio: 'I am user 2',
-      theme: 'CCCccc',
+      avatar_token_id: "0x02",
+      name: "user2",
+      bio: "I am user 2",
+      theme: "CCCccc",
       links: [
-        { key: 'hello', value: 'world' },
-        { key: 'hello2', value: 'world2' }
-      ]
+        { key: "hello", value: "world" },
+        { key: "hello2", value: "world2" },
+      ],
     });
 
-    res = await profileContract.functions.update_profile({
+    res = await profileContract.functions.update_profile(
+      {
+        address: user1.address,
+        profile: {
+          avatar_contract_id: collectionContract.getId(),
+          avatar_token_id: "0x01",
+          name: "user1",
+          bio: "I am user one",
+          theme: "000",
+          links: [],
+        },
+      },
+      {
+        beforeSend: async (tx) => {
+          await user1.signer.signTransaction(tx);
+        },
+      }
+    );
+
+    await res.transaction?.wait();
+
+    res = await profileContract.functions.get_profile({
       address: user1.address,
-      profile: {
-        avatar_contract_id: collectionContract.getId(),
-        avatar_token_id: '0x01',
-        name: 'user1',
-        bio: 'I am user one',
-        theme: '000',
-        links: [
-        ]
-      }
-    }, {
-      beforeSend: async (tx) => {
-        await user1.signer.signTransaction(tx);
-      }
-    });
-
-    await res.transaction?.wait();
-
-    res = await profileContract.functions.get_profile({
-      address: user1.address
     });
 
     expect(res.result).toEqual({
       avatar_contract_id: collectionContract.getId(),
-      avatar_token_id: '0x01',
-      name: 'user1',
-      bio: 'I am user one',
-      theme: '000',
-      links: [
-      ]
+      avatar_token_id: "0x01",
+      name: "user1",
+      bio: "I am user one",
+      theme: "000",
+      links: [],
     });
   });
 
-  it('should not set a profile', async () => {
+  it("should not set a profile", async () => {
     expect.assertions(4);
 
     try {
-      await profileContract.functions.update_profile({
-        profile: {
-          avatar_contract_id: collectionContract.getId(),
-          avatar_token_id: '0x01',
-          name: 'user1',
-          bio: 'I am user one',
-          theme: '000',
-          links: [
-          ]
+      await profileContract.functions.update_profile(
+        {
+          profile: {
+            avatar_contract_id: collectionContract.getId(),
+            avatar_token_id: "0x01",
+            name: "user1",
+            bio: "I am user one",
+            theme: "000",
+            links: [],
+          },
+        },
+        {
+          beforeSend: async (tx) => {
+            await user1.signer.signTransaction(tx);
+          },
         }
-      }, {
-        beforeSend: async (tx) => {
-          await user1.signer.signTransaction(tx);
-        }
-      });
+      );
     } catch (error) {
-      expect(JSON.parse(error.message).error).toStrictEqual('missing "address" argument');
+      expect(JSON.parse(error.message).error).toStrictEqual(
+        'missing "address" argument'
+      );
     }
 
     try {
-      await profileContract.functions.update_profile({
-        address: user1.address,
-      }, {
-        beforeSend: async (tx) => {
-          await user1.signer.signTransaction(tx);
+      await profileContract.functions.update_profile(
+        {
+          address: user1.address,
+        },
+        {
+          beforeSend: async (tx) => {
+            await user1.signer.signTransaction(tx);
+          },
         }
-      });
+      );
     } catch (error) {
-      expect(JSON.parse(error.message).error).toStrictEqual('missing "profile" argument');
+      expect(JSON.parse(error.message).error).toStrictEqual(
+        'missing "profile" argument'
+      );
     }
 
     try {
-      await profileContract.functions.update_profile({
-        address: user1.address,
-        profile: {
-          avatar_contract_id: collectionContract.getId(),
-          avatar_token_id: '0x01',
-          name: 'user1',
-          bio: 'I am user one',
-          theme: '0000',
-          links: [
-          ]
+      await profileContract.functions.update_profile(
+        {
+          address: user1.address,
+          profile: {
+            avatar_contract_id: collectionContract.getId(),
+            avatar_token_id: "0x01",
+            name: "user1",
+            bio: "I am user one",
+            theme: "0000",
+            links: [],
+          },
+        },
+        {
+          beforeSend: async (tx) => {
+            await user1.signer.signTransaction(tx);
+          },
         }
-      }, {
-        beforeSend: async (tx) => {
-          await user1.signer.signTransaction(tx);
-        }
-      });
+      );
     } catch (error) {
-      expect(JSON.parse(error.message).error).toStrictEqual('"theme" argument must be composed of 3 or 6 characters.');
+      expect(JSON.parse(error.message).error).toStrictEqual(
+        '"theme" argument must be composed of 3 or 6 characters.'
+      );
     }
 
     try {
-      await profileContract.functions.update_profile({
-        address: user1.address,
-        profile: {
-          avatar_contract_id: collectionContract.getId(),
-          avatar_token_id: '0x01',
-          name: 'user1',
-          bio: 'I am user one',
-          theme: '00J',
-          links: [
-          ]
+      await profileContract.functions.update_profile(
+        {
+          address: user1.address,
+          profile: {
+            avatar_contract_id: collectionContract.getId(),
+            avatar_token_id: "0x01",
+            name: "user1",
+            bio: "I am user one",
+            theme: "00J",
+            links: [],
+          },
+        },
+        {
+          beforeSend: async (tx) => {
+            await user1.signer.signTransaction(tx);
+          },
         }
-      }, {
-        beforeSend: async (tx) => {
-          await user1.signer.signTransaction(tx);
-        }
-      });
+      );
     } catch (error) {
-      expect(JSON.parse(error.message).error).toStrictEqual('"J" is not a valid hex character');
+      expect(JSON.parse(error.message).error).toStrictEqual(
+        '"J" is not a valid hex character'
+      );
     }
   });
 
-  it('should not get avatar or name', async () => {
+  it("should not get avatar or name", async () => {
     // transfer NFT 0x01 to user2
-    let res = await collectionContract.functions.transfer({
-      from: user1.address,
-      to: user2.address,
-      token_id: '0x01',
-    }, {
-      beforeSend: async (tx) => {
-        await user1.signer.signTransaction(tx);
+    let res = await collectionContract.functions.transfer(
+      {
+        from: user1.address,
+        to: user2.address,
+        token_id: "0x01",
+      },
+      {
+        beforeSend: async (tx) => {
+          await user1.signer.signTransaction(tx);
+        },
       }
-    });
+    );
 
     await res.transaction?.wait();
 
     res = await profileContract.functions.get_profile({
-      address: user1.address
+      address: user1.address,
     });
 
     // avater info should be blank
     expect(res.result).toEqual({
-      avatar_contract_id: '',
-      avatar_token_id: '0x',
-      name: 'user1',
-      bio: 'I am user one',
-      theme: '000',
-      links: [
-      ]
+      avatar_contract_id: "",
+      avatar_token_id: "0x",
+      name: "user1",
+      bio: "I am user one",
+      theme: "000",
+      links: [],
     });
 
     // transfer name user2 to user1
-    res = await nameserviceContract.functions.transfer({
-      from: user2.address,
-      to: user1.address,
-      token_id: encodeHex('user2'),
-    }, {
-      beforeSend: async (tx) => {
-        await user2.signer.signTransaction(tx);
+    res = await nameserviceContract.functions.transfer(
+      {
+        from: user2.address,
+        to: user1.address,
+        token_id: encodeHex("user2"),
+      },
+      {
+        beforeSend: async (tx) => {
+          await user2.signer.signTransaction(tx);
+        },
       }
-    });
+    );
 
     await res.transaction?.wait();
 
     res = await profileContract.functions.get_profile({
-      address: user2.address
+      address: user2.address,
     });
 
     // name info should be blank
     expect(res.result).toEqual({
       avatar_contract_id: collectionContract.getId(),
-      avatar_token_id: '0x02',
-      bio: 'I am user 2',
-      theme: 'CCCccc',
+      avatar_token_id: "0x02",
+      bio: "I am user 2",
+      theme: "CCCccc",
       links: [
-        { key: 'hello', value: 'world' },
-        { key: 'hello2', value: 'world2' }
-      ]
+        { key: "hello", value: "world" },
+        { key: "hello2", value: "world2" },
+      ],
     });
   });
 });
