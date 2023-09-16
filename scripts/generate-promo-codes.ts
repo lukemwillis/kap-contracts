e5ebbbd99d4d60d0eca7349a0c42a2d3a75cf582
luke <luke@prescryptive.com>
2023-09-16 07:27:10 -0700

    batches

--- scripts/generate-promo-codes.ts
+++ scripts/generate-promo-codes.ts
@@ -8,7 +8,7 @@ const result = {
   codes: [],
 };
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 10000; i++) {
   const code = randomUUID();
   const hash = createHash("sha256").update(code).digest();
   result.codes.push({
@@ -66,6 +66,7 @@ async function processBatch() {
   console.log(`batch sent with tx id ${transaction.id}`);
   await transaction.wait().then((receipt) => {
     console.log(`batch included in block ${receipt.blockId}`);
+    processBatch();
   });
 }
 
