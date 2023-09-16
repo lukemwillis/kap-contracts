7339a2caead44cd675418f28123e04a08df0c0d7
luke <luke@prescryptive.com>
2023-09-16 07:23:56 -0700

    multiple ops in one tx

--- scripts/generate-promo-codes.ts
+++ scripts/generate-promo-codes.ts
@@ -19,12 +19,6 @@ for (let i = 0; i < 100; i++) {
 
 console.log(result);
 
-// const { config } = require("dotenv");
-// config(); // Load environment variables from .env file
-
-// const koinDomainAbi = require("./koindomain-abi.json");
-// const promoCodes = require("./promo-codes.json");
-
 const WIF = process.env.WIF; // Read WIF from environment variable
 const CONTRACT_ID = process.env.CONTRACT_ID;
 const JSON_RPC = process.env.JSON_RPC;
@@ -48,25 +42,30 @@ const contract = new Contract({
 
 let current = 0;
 async function processBatch() {
-  const txs = [];
-  for (let i = 0; i < 1 && current < result.codes.length; i++, current++) {
+  const operations = [];
+  for (let i = 0; i < 100 && current < result.codes.length; i++, current++) {
     const { code, hash } = result.codes[current];
-    txs.push(contract.functions.add_promo_code(
+    console.log(`Adding promo code ${code} with hash ${hash}`);
+    const { operation } = await contract.functions.add_promo_code(
       {
         hashed_promo_code: hash,
       },
       {
-      //   payer: process.env.CRON_PAYER,
-        rcLimit: "50000000",
+        onlyOperation: true,
       }
-    ).then(({ transaction }) => {
-      console.log(`${transaction.id} sent for ${code}`);
-      return transaction.wait("byTransactionId", 90000).then((res) => { console.log(`${transaction.id} included in ${res.blockId}`); });
-    }));
+    );
+    operations.push(operation);
   }
-  await Promise.all(txs).then(() => {
-    console.log("finished batch");
-    processBatch();
+  const tx = await signer.prepareTransaction({
+    header: {
+      rc_limit: "10000000000",
+    },
+    operations,
+  });
+  const { transaction } = await signer.sendTransaction(tx);
+  console.log(`batch sent with tx id ${transaction.id}`);
+  await transaction.wait().then((receipt) => {
+    console.log(`batch included in block ${receipt.blockId}`);
   });
 }
 
